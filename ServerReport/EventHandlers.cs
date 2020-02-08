69cd5cf555fa8f772df61f0e468f495a781b8fdc
Vyrezz <m.r.eilert@st.hanze.nl>
2020-02-08 16:53:11 +0100

    Initial Upload

---
+++ ServerReport/EventHandlers.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using EXILED;
+using Grenades;
+using MEC;
+using Mirror;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Net.Http;
+
+namespace ServerReports
+{
+    public class EventHandlers
+    {
+        public Plugin plugin;
+        private string pingPongRoles;
+
+        public EventHandlers(Plugin plugin) => this.plugin = plugin;
+
+        public void OnCheaterReport(ref CheaterReportEvent ev)
+        {
+            ev.Allow = false;
+            ReferenceHub reportedPlayer = Plugin.GetPlayer(ev.ReportedId);
+            ReferenceHub reportedBy = Plugin.GetPlayer(ev.ReportedId);
+
+            Webhook webhk = new Webhook(plugin.WebhookURL);
+
+            List<Embed> listEmbed = new List<Embed>();
+          
+
+            EmbedField reporterName = new EmbedField();
+            reporterName.Name = "Report By";
+            reporterName.Value = Plugin.GetPlayer(ev.ReporterId).nicknameSync.MyNick;
+            reporterName.Inline = true;
+
+            EmbedField reporterUserID = new EmbedField();
+            reporterUserID.Name = "Reporter UserID";
+            reporterUserID.Value = Plugin.GetPlayer(ev.ReporterId).characterClassManager.UserId;
+            reporterUserID.Inline = true;
+
+            EmbedField reportedName = new EmbedField();
+            reportedName.Name = "Reported Player";
+            reportedName.Value = Plugin.GetPlayer(ev.ReportedId).nicknameSync.MyNick;
+            reportedName.Inline = true;
+
+            EmbedField reportedUserID = new EmbedField();
+            reportedUserID.Name = "Reported Player";
+            reportedUserID.Value = Plugin.GetPlayer(ev.ReportedId).characterClassManager.UserId;
+            reportedUserID.Inline = true;
+
+            EmbedField Reason = new EmbedField();
+            Reason.Name = "Report Reason";
+            Reason.Value = ev.Report;
+            Reason.Inline = true;
+
+            Embed embed = new Embed();
+            embed.Title = "New In-game Report";
+            embed.Fields.Add(reporterName);
+            embed.Fields.Add(reporterUserID);
+            embed.Fields.Add(reportedName);
+            embed.Fields.Add(reportedUserID);
+            embed.Fields.Add(Reason);
+            
+
+            listEmbed.Add(embed);
+
+           
+            if (string.IsNullOrWhiteSpace(plugin.RoleIDsToPing)) webhk.Send(plugin.CustomMessage, null, null, false, embeds: listEmbed);
+            else
+            {
+                if (!plugin.RoleIDsToPing.Contains(','))
+                {
+                    webhk.Send("<@&" + plugin.RoleIDsToPing + "> " + plugin.CustomMessage, null, null, false, embeds: listEmbed);
+                }
+                else
+                {
+                    string[] split = plugin.RoleIDsToPing.Split(',');
+                    foreach (string roleid in split)
+                    {
+                        pingPongRoles += $"<@&{roleid}> ";
+                    }
+                    webhk.Send(pingPongRoles + "" + plugin.CustomMessage, null, null, false, embeds: listEmbed);
+                }
+            }
+        }
+    }
+}
---
+++ ServerReport/Plugin.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using EXILED;
+
+
+namespace ServerReports
+{
+	public class Plugin : EXILED.Plugin
+	{
+		public EventHandlers EventHandlers;
+        public string WebhookURL;
+        public string RoleIDsToPing;
+        public string CustomMessage;
+
+		
+		public override void OnEnable()
+		{
+			try
+			{
+                WebhookURL = Config.GetString("srvreport_webhook", "");
+                RoleIDsToPing = Config.GetString("srvreport_roleids", "");
+                CustomMessage = Config.GetString("srvreport_custom_message", "A new in-game report has been made!");
+
+                EventHandlers = new EventHandlers(this);
+                Events.CheaterReportEvent += EventHandlers.OnCheaterReport;
+                Info($"Report INtegration Loaded");
+			}
+			catch (Exception e)
+			{
+				Error($"There was an error loading the plugin: {e}");
+			}
+		}
+
+        public override void OnDisable()
+		{
+            Events.CheaterReportEvent -= EventHandlers.OnCheaterReport;
+            EventHandlers = null;
+		}
+
+		public override void OnReload()
+		{
+            //haha no
+		}
+
+		public override string getName { get; } = "Server Reports to Discord";
+	}
+}
